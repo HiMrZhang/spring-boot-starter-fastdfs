f7713d83b6240bc53c905f0dff3342a641facfc1
zhangyunpeng <2305658511@qq.com>
2019-04-16 16:14:57 +0800

    bug fix

--- src/main/java/com/easysoft/fastdfs/autoconfigure/FastDFSClietAutoConfiguration.java
+++ src/main/java/com/easysoft/fastdfs/autoconfigure/FastDFSClietAutoConfiguration.java
@@ -1,17 +1,18 @@
 package com.easysoft.fastdfs.autoconfigure;
 
 import com.easysoft.fastdfs.FastDFSClient;
+import com.easysoft.fastdfs.IFastDFSClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConditionalOnMissingBean({FastDFSClient.class})
+@ConditionalOnMissingBean({IFastDFSClient.class})
 @EnableConfigurationProperties(FastDFSProperties.class)
-public class FastDFSAutoConfiguration {
+public class FastDFSClietAutoConfiguration {
     @Bean
-    public FastDFSClient hdfsClient(FastDFSProperties fastDFSProperties) {
+    public IFastDFSClient fastDFSClient(FastDFSProperties fastDFSProperties) {
         return new FastDFSClient(fastDFSProperties);
     }
 }
